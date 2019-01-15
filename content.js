c74c52cca9ad74f5ab51c7d46ec280c6b9ed10ca
Ben Glasser <ben@benglasser.com>
2019-01-15 11:51:37 -0700

    update to work with latest dev branch of stardust

--- content.js
+++ content.js
@@ -1,9 +1,14 @@
 var removed = false
 var removeBanner = () => {
   var banner = document.querySelector('[class^="TopBar__Container"]')
+  var newBanner = Array.prototype.slice.call(document.getElementsByTagName('span')).filter(span => span.innerHTML === 'Activate your physical card in the mobile app.')[0]
   if (!!banner) {
     removed = true
     banner.remove()
+  }
+  if (!!newBanner) {
+    removed = true
+    newBanner.parentElement.parentElement.parentElement.remove()
   }
   if (removed) clearInterval(intervalId)
 }
